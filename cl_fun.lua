7b59aefe812dea6a21c893efc6dee16f5702d4af
EzKliks <bastien.ranc.pro@gmail.com>
2017-10-31 12:46:21 +0100

    Modification sur les spawns, non aléatoire et a l'aéroport

--- cl_fun.lua
+++ cl_fun.lua
@@ -1,7 +1,7 @@
 RegisterNetEvent('VehicleSpawn')
 AddEventHandler('VehicleSpawn', function(Vehicle)
 
-	TriggerEvent("chatMessage", "SYSTEM", { 255,0,0}, Vehicle)
+	TriggerEvent("chatMessage", "SYSTEM", {255,0,0}, "Vous avez fais apparaître un/une "..Vehicle)
 
   	local hash = GetHashKey(Vehicle)
 
@@ -14,15 +14,60 @@ AddEventHandler('VehicleSpawn', function(Vehicle)
 
 	local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), false))
 	local vehicle = CreateVehicle(hash, x + 2, y + 2, z + 1, 0.0, true, false)
+	SetVehicleNumberPlatetext(vehicle, Vehicle)
 end)
 
-function stringsplit(self, delimiter)
-  local a = self:Split(delimiter)
-  local t = {}
+RegisterNetEvent('repairCar')
+AddEventHandler('repairCar', function() 
+	local currentVehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
+	SetVehicleFixed(currentVehicle)
+end)
 
-  for i = 0, #a - 1 do
-     table.insert(t, a[i])
-  end
+RegisterNetEvent('colorCar')
+AddEventHandler('colorCar', function(colora, colorb)  
+	local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
+	if(vehicle == nil) then
+		TriggerEvent("chatMessage", "SYSTEM", {255,0,0}, "Vous devez être dans un vehicle")
+	else
+		TriggerEvent("chatMessage", "SYSTEM", {255,0,0}, colora.."|"..colorb)
+		SetVehicleColours(vehicle, colora, colorb)
+	end
+end)
 
-  return t
+RegisterNetEvent('changeModel')
+AddEventHandler('changeModel', function(idmodel) 
+
+	TriggerEvent("chatMessage", "SYSTEM", {255,0,0}, "changement de skin ...("..idmodel..")")
+
+	if DoesEntityExist(GetPlayerPed(-1)) then
+	local model = GetHashKey(idmodel)
+	RequestModel(model)
+	while not HasModelLoaded(model) do
+		Citizen.Wait(0)
+	end
+	SetPlayerModel(PlayerId(), model)
+	SetModelAsNoLongerNeeded(model)
 end
+end)
+
+RegisterNetEvent('giveGun')
+AddEventHandler('giveGun', function(nameG) 
+	GiveWeaponToPed(GetPlayerPed(-1), GetHashKey(nameG), 1000, false)
+end)
+
+RegisterNetEvent('bourre')
+AddEventHandler('bourre', function(param)
+	SetPedIsDrunk(GetPlayerPed(-1), param)
+end)
+
+RegisterNetEvent('giveDiscord')
+AddEventHandler('giveDiscord', function()
+	TriggerEvent("chatMessage", "SYSTEM", {255,0,0}, "Discord : https://discord.gg/sZmnH9X")
+end)
+
+RegisterNetEvent('getPos')
+AddEventHandler('getPos', function()
+	local pos = GetEntityCoords(GetPlayerPed(-1))
+	local string = "{x = " .. pos.x .. ",y = " .. pos.y .. ",z = " .. pos.z-1 .. " },\n"
+	TriggerEvent('chatMessage', 'SYSTEM', {255, 0, 0}, 'Your position is :'..string);
+end)
--- sv_fun.lua
+++ sv_fun.lua
@@ -3,4 +3,32 @@ AddEventHandler('chatMessage', function(player, color, message)
         TriggerClientEvent('VehicleSpawn', player, message:sub(8))
         CancelEvent()
     end
+    if message:sub(1) == '/discord' then
+    	TriggerClientEvent('giveDiscord', player)
+    	CancelEvent()
+    end
+    if message:sub(1) == '/repair' then
+    	TriggerClientEvent('repairCar', player)
+    	CancelEvent()
+    end
+    if message:sub(1,6) == '/color' then
+    	TriggerClientEvent('colorCar', player, message:sub(8,9), message:sub(11,12))
+    	CancelEvent()
+    end
+    if message:sub(1, 5) == '/skin' then
+    	TriggerClientEvent('changeModel', player, message:sub(7))
+    	CancelEvent()
+	end
+	if message:sub(1,7) == '/bourre' then
+		TriggerClientEvent('bourre', player, message:sub(9))
+		CancelEvent()
+	end
+	if message:sub(1,4) == '/gun' then
+		TriggerClientEvent('giveGun', player, message:sub(6))
+		CancelEvent()
+	end
+    if message:sub(1) == '/getPos' then
+        TriggerClientEvent('getPos', player)
+        CancelEvent()
+    end
 end)
